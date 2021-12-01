34c3dffa98d2af25dc4a26356948ae5bf8713472
CarbonDRFT <Danier-1995@hotmail.com>
2021-12-01 16:58:53 +0100

    update

---
+++ semester-project/src/api/header-video/controllers/header-video.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  header-video controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::header-video.header-video');
