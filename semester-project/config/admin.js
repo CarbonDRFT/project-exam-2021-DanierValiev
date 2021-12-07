43b9dbe60bfcc215a09f39ed3d1076d45f1d4b8c
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2021-12-07 16:01:35 +0100

    Delete semester-project directory

--- semester-project/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '820bd4c4643881e122aba1bcd9a55f70'),
-  },
-});
--- semester-project/src/api/header-video/controllers/header-video.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  header-video controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::header-video.header-video');
